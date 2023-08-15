3b5a66cf636c4f20cc12361572ff28d139430759
ART0059179 <fernandofalcaoscot.cpv@gmail.com>
2023-08-15 10:46:47 -0300

    commit do dia 15

--- lista041/questao11.py
+++ lista041/questao11.py
@@ -7,4 +7,4 @@ a = int(input("insira aqui um número inteiro de 3 algarismos:"))
 b = int (a / 100)
 
 if (a >= 100):
-    print(f"A casa de centena desse número é: {b}")
+    print(f"O algarismo da casa de centena desse número é: {b}")
--- lista041/questao13.py
+++ lista041/questao13.py
@@ -6,4 +6,15 @@ a = float(input("Insira aqui o primeiro número:"))
 b = float(input("Insira aqui o segundo número:"))
 c = float(input("Insira aqui o terceiro número:"))
 
-if (a > b and a > c amd a >)
+if a <= b and a <= c and b <= c:
+    print(f'A ordem crescente é {a} , {b} e {c}')
+elif a <= b and a <=c and c <= b:
+    print(f'A ordem crescente é {a} , {c} e {b}')
+elif b <= a and b <= c and a <= c:
+    print(f'A ordem crescente é {b} , {a} e {c}')
+elif b <= a and b <= c and c <= a:
+    print(f'A ordem crescente é {b} , {c} e {a}')
+elif c <= a and c <= b and a <=b:
+    print(f'A ordem crescente é {c} , {a} e {b}')
+elif c <= a and c <= b and b <= a:
+    print(f'A ordem crescente é {c} , {b} e {a}')
