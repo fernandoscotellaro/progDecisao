a0e9bffcab39f13ee9c6c6ca2c06842462a3ee58
ART0059179 <fernandofalcaoscot.cpv@gmail.com>
2023-08-16 11:19:46 -0300

    Python Lista 04.2

--- lista042/questao08.py
+++ lista042/questao08.py
@@ -2,16 +2,16 @@
 8.	Fazer um algoritmo que pergunte 3 números, e ao final, guarde na variável o maior destes 3 números inseridos. O valor da variável maior deverá ser exibido ao final.
 '''
 
-num1 = int(input("Insira um número:"))
+numum = int(input("Insira um número:"))
 num2 = int(input("Insira outro número:"))
 num3 = int(input("Insira mais um número:"))
 
-maior = num1
+maior = numum
 
-if (num1 < num2):
+if (numum < num2):
     maior = num2
 
-if (num1 < num3):
+if (numum < num3):
     maior = num3
 
 print(f"O maior número é {maior}")
